4a12aded2f9740294d39ee377ad617ad510b2617
Bräñdôñ Lydëñ <95022425+brandonladen@users.noreply.github.com>
2023-06-22 11:57:11 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+* main - Checks the code
+* @argv: 
+* @argc:
+* Return: 0
+*/
+int main(int argc, char argv[])
+{
+        if (argc != 2)
+         {
+                  fprintf("txt");
+          }
+  
+         file_handler(argv[1]);
+  return (0);
+}
