4fc3375c2d2493f95f8ca9162f65cb5b69d7259d
brandonladen <brandonladen486@gmail.com>
2023-06-22 04:53:03 -0700

    added a prototype in header file

--- monty.h
+++ monty.h
@@ -35,6 +35,7 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 int file_handler(char *filename);
 
 #endif /* MAIN */
