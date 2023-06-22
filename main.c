06ddb38367108dcf4e0d4f1b7e8c6978b4a4be30
brandonladen <brandonladen486@gmail.com>
2023-06-22 04:22:51 -0700

    created header file

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include "monty.h"
 /**
-* main - Checks the code
-* @argv: 
-* @argc:
-* Return: 0
-*/
-int main(int argc, char argv[])
+ * main - Checks the code
+ * @argv: 
+ * @argc:
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
         if (argc != 2)
-         {
-                  fprintf("txt");
-          }
-  
-         file_handler(argv[1]);
+	{
+		fprintf("USAGE: monty file");
+		return (EXIT_FAILURE);
+	}
+
+	file_handler(argv[1]);
   return (0);
 }
